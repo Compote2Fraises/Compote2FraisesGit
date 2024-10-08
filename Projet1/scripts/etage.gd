cb399dd50d518db4aa4ba10846bcd92edc530339
RomanHlt <romanhalatre@gmail.com>
2024-10-08 19:51:59 +0200

    Tunnel Fonctionnel

--- Projet1/scripts/etage.gd
+++ Projet1/scripts/etage.gd
@@ -4,16 +4,19 @@ extends Node2D
 var WallColisions
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Ground.z_index = Level-1
-	$Water.z_index = Level
-	$"Walls 1".z_index = Level+1
-	$Nature.z_index = Level+1
-	$Props.z_index = Level+1
+	$Ground.z_index = Level*3
+	$Border.z_index = Level*3 + 1
+	$Water.z_index = Level*3 + 1
+	$"Walls 1".z_index = Level*3+2
+	$Nature.z_index = Level*3 + 2
+	$Props.z_index = Level*3 + 2
 
 	print("--- Etage n°",Level," ---")
-	$"Walls 1".tile_set.set_physics_layer_collision_layer(0,Level | Level+1)
+	$"Walls 1".tile_set.set_physics_layer_collision_layer(0,Level+1)
 	print("Walls 1: Layer n°",$"Walls 1".z_index,"/ Physics: ",$"Walls 1".tile_set.get_physics_layer_collision_layer(0))
-	$Props.tile_set.set_physics_layer_collision_layer(0,Level)
+	$"Border".tile_set.set_physics_layer_collision_layer(0,Level+1)
+	print("Border: Layer n°",$"Border".z_index,"/ Physics: ",$"Border".tile_set.get_physics_layer_collision_layer(0))
+	$Props.tile_set.set_physics_layer_collision_layer(0,Level+1)
 	print("Props: Layer n°",$Props.z_index,"/ Physics: ",$"Props".tile_set.get_physics_layer_collision_layer(0))
-	$Nature.tile_set.set_physics_layer_collision_layer(0,Level)
+	$Nature.tile_set.set_physics_layer_collision_layer(0,Level+1)
 	print("Nature: Layer n°",$Props.z_index,"/ Physics: ",$"Nature".tile_set.get_physics_layer_collision_layer(0))
--- Projet1/scripts/stairs.gd
+++ Projet1/scripts/stairs.gd
@@ -29,13 +29,13 @@ func _ready() -> void:
 	lvl = get_parent().get_parent().Level
 
 func _on_up_body_entered(body):
-	if body.name == "Player" and body.z_index == lvl+1:
-		body.collision_mask = int(lvl) + 1 # mask joueur = etage de l'escalier + 1
-		body.z_index = int(lvl + 2)
+	if body.name == "Player" and body.z_index == lvl*3+2:
+		body.collision_mask = int(lvl+2) # mask joueur = etage de l'escalier + 1
+		body.z_index = int((lvl+1)*3+2)
 		print("Etage Supérieur : ", lvl+1)
 
 func _on_down_body_entered(body):
-	if body.name == "Player" and body.z_index == lvl+2:
-		body.collision_mask = int(lvl) # mask joueur = etage de l'escalier
-		body.z_index = int(lvl+1)
-		print("Etage Inférieur : ", lvl)
+	if body.name == "Player" and body.z_index == (lvl+1)*3+2:
+		body.collision_mask = int(lvl+1) # mask joueur = etage de l'escalier
+		body.z_index = int(lvl*3+2)
+		print("Etage Inférieur : ",lvl)
